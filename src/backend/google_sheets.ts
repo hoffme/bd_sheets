69359d018abe1264f92aba8491440db24e714ee1
Juan Ignacio Coronel <phjocoronel@gmail.com>
2024-06-09 06:30:20 -0300

    feat: connect google apis

--- src/backend/google_sheets.ts
+++ src/backend/google_sheets.ts
@@ -1,23 +1,37 @@
-const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSA_ilnVV7YDF4RPF01B6tccgbsiZq3J8cZ_MCkXtk9EY8cvg_BqL4gp0Gs_TlwJ6PlhZ0A4Jh0OPm7/pub?output=csv";
+import { google } from "googleapis";
 
 export const getGoogleSheetsData = async () => {
-    const response = await fetch(url);
-    const text = await response.text();
+    const GOOGLE_CREDENTIALS = process.env.GOOGLE_CREDENTIALS || ''
+    const SPREAD_SHEETS_ID = process.env.SPREAD_SHEETS_ID || ''
+    const SPREAD_SHEETS_RANGE = process.env.SPREAD_SHEETS_RANGE || ''
 
-    return text.split('\r\n').slice(1).map((txt) => {
-        const cells = txt.split(',')
-        return {
-            company: cells[0],
-            product: cells[1],
-            efficacy: cells[2],
-            effectiveness: cells[3],
-            ethics: cells[4],
-            equity: cells[5],
-            environment: cells[6],
-            status: cells[7],
-            country: cells[8],
-            url: cells[9],
-            totalScore: cells[10]
-        }
+    const auth = new google.auth.GoogleAuth({
+        credentials: JSON.parse(atob(GOOGLE_CREDENTIALS)),
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    })
+
+    const sheets = google.sheets({ version: 'v4', auth });
+    const res = await sheets.spreadsheets.values.get({
+        spreadsheetId: SPREAD_SHEETS_ID,
+        range: SPREAD_SHEETS_RANGE
     });
+    if (!res.data.values) {
+        throw new Error('error to get data from google sheets');
+    }
+
+    const data = res.data.values.slice(1).map((cells) => ({
+        company: cells[0],
+        product: cells[1],
+        efficacy: cells[2],
+        effectiveness: cells[3],
+        ethics: cells[4],
+        equity: cells[5],
+        environment: cells[6],
+        status: cells[7],
+        country: cells[8],
+        url: cells[9],
+        totalScore: cells[10]
+    }));
+
+    return data;
 }
